d2f40c5e7d22b696adca06b13373f1ddf60dc79c
Chris <chris@macbooks-MacBook-Pro.local>
2018-03-10 18:42:53 +0000

    updated

--- dry/dry/settings/base.py
+++ dry/dry/settings/base.py
@@ -15,7 +15,6 @@ from __future__ import absolute_import, unicode_literals
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from dotenv import load_dotenv, find_dotenv
-
 load_dotenv(find_dotenv())
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- dry/dry/settings/production.py
+++ dry/dry/settings/production.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from .base import *
 
 DEBUG = False
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = ["*"]
 
 try:
